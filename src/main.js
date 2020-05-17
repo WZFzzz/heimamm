be525a4dc9fe27772a5b8bff1d8bb139ab79a59a
WZFzzz <1824167348@qq.com>
2020-05-17 16:24:19 +0800

    5-完成了表单基本规则得校验

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import router from '@/router'
+//导入element.js
+import '@/plugins/element.js'
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/views/layout/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>12311516151</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
